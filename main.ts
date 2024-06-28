215e53db69b7be1f80f7c40cbe6b552a9ba40402
Pavel Murashkin <pashamuruskinn@gmail.com>
2024-06-28 12:29:51 +0300

    Before push

--- main.ts
+++ main.ts
@@ -21,83 +21,146 @@ const port = 3000
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
+// app.all('/', upload.fields([{ name: 'file1' }, { name: 'file2' }]), async (req: Request & { files: { file1: Express.Multer.File[], file2: Express.Multer.File[] } }, res: Response) => {
+//     if (req.method === 'GET') {
+//         res.send(FormPage());
+//     } else if (req.method === 'POST') {
+//         const { text } = req.body;
+//         const file1Buffer = (req.files['file1'][0] as Express.Multer.File).buffer;
+//         const file2Buffer = (req.files['file2'][0] as Express.Multer.File).buffer;
+//
+//         if (!text || !file1Buffer || !file2Buffer) {
+//             return res.send(
+//                 `<h1>Error: Missing fields</h1>
+//             ${FormPage()}`
+//         );
+//         }
+//
+//         const file1Base64 = file1Buffer.toString('base64');
+//         const file2Base64 = file2Buffer.toString('base64');
+//
+//         if (!isTextFile(file1Base64.toString()) || !isTextFile(file2Base64.toString())) {
+//             return res.send(
+//                 `<h1>Error: Files are not text files</h1>
+//             ${FormPage()}`
+//         );
+//         }
+//
+//         const textFromFile1 = Buffer.from(file1Base64, 'base64').toString('utf-8');
+//         const textFromFile2 = Buffer.from(file2Base64, 'base64').toString('utf-8');
+//
+//         const similarityScore = calculateSimilarity(textFromFile1, textFromFile2);
+//
+//         const resultText = `Для запроса "${text}" сходство (расстояние) равно ${similarityScore}`;
+//
+//         try {
+//             await fs.promises.writeFile('Ответ на запрос.txt', resultText);
+//             res.download('Ответ на запрос.txt');
+//         } catch (err) {
+//             console.error(err);
+//             res.status(500).send('Error saving the file');
+//         }
+//     } else {
+//         res.status(405).send('Method Not Allowed');
+//     }
+// });
 
-
-app.get('/', ((req: Request, res: Response) => {
-    res.send(FormPage());
-}))
-
-app.post('/', upload.fields([{ name: 'file1' }, { name: 'file2' }]),
-     async (req: Request & { files: { file1: Express.Multer.File[], file2: Express.Multer.File[] } },
-           res: Response) => {
+app.all('/', upload.fields([{ name: 'file1' }, { name: 'file2' }]),
+    async (req: Request & { files: { file1: Express.Multer.File[], file2: Express.Multer.File[] } }, res: Response) => {
+    if (req.method === 'GET') {
+        res.send(FormPage());
+    } else if (req.method === 'POST') {
         const { text } = req.body;
-        const file1Buffer = (req.files['file1'][0] as Express.Multer.File).buffer;
-        const file2Buffer = (req.files['file2'][0] as Express.Multer.File).buffer;
+        // const file1Buffer = (req.files['file1'][0] as Express.Multer.File).buffer;
+        // const file2Buffer = (req.files['file2'][0] as Express.Multer.File).buffer;
+        const file1 = req.files['file1'];
+        const file2 = req.files['file2'];
 
-        if (!text || !file1Buffer || !file2Buffer) {
+        // if (!text || !file1Buffer || !file2Buffer) {
+        //     return res.send(
+        //         `<h1>Error: Missing fields</h1>
+        //     ${FormPage()}`
+        // );
+        // }
+        // if (!text || !file1Buffer || !file2Buffer) {
+        //     return res.send(
+        //         `${FormPage()}
+        //         <h1>Error: Missing fields</h1>`
+        // );
+        // }
+        if (!text || !file1 || !file1[0] || !file1[0].buffer || !file2 || !file2[0] || !file2[0].buffer) {
             return res.send(
-                `<h1>Error: Missing fields</h1>
-            ${FormPage()}`
+                `<div style="display: flex; flex-direction: column; align-items: center;">
+            <div id="error-message" style="border: 5px solid red; padding: 10px; margin-top: 10px;">Error: Missing fields</div>
+        </div>
+        ${FormPage()}
+        <script>
+            document.addEventListener('DOMContentLoaded', function() {
+                const errorMessage = document.getElementById('error-message');
+                errorMessage.innerText = 'Error: Files are not text files';
+            });
+        </script>
+`
         );
         }
 
-        const file1Base64 = file1Buffer.toString('base64');
-        const file2Base64 = file2Buffer.toString('base64');
+        const file1Buffer = file1[0].buffer;
+        const file2Buffer = file2[0].buffer;
 
-        if (!isTextFile(file1Base64.toString()) || !isTextFile(file2Base64.toString())) {
-            return res.send(
-                `<h1>Error: Files are not text files</h1>
-            ${FormPage()}`
-        );
-        }
-
-        const textFromFile1 = Buffer.from(file1Base64, 'base64').toString('utf-8');
-        const textFromFile2 = Buffer.from(file2Base64, 'base64').toString('utf-8');
+        const textFromFile1 = file1Buffer.toString('utf-8');
+        const textFromFile2 = file2Buffer.toString('utf-8');
 
         const similarityScore = calculateSimilarity(textFromFile1, textFromFile2);
 
-        const resultText = `Для запроса "${text}" сходство (расстояние) равно ${similarityScore}`;
+        const resultText = `Для запроса "${text}" сходство (расстояние) равно ${similarityScore};`
 
-         try {
-             await fs.promises.writeFile('Ответ на запрос.txt', resultText);
-             res.download('Ответ на запрос.txt');
-         } catch (err) {
-             console.error(err);
-             res.status(500).send('Error saving the file');
-         }
-    });
-
-
-app.get('/api', ((req: Request, res: Response) => {
-    res.json(JsonResponse());
-}))
-
-app.post('/api', (req: Request, res: Response) => {
-    const jsonData = req.body;
-    if (!jsonData.text || !jsonData.file1 || !jsonData.file2) {
-        return res.status(400).json({ error: 'Отсутствуют необходимые поля' });
+        try {
+            await fs.promises.writeFile('Ответ на запрос.txt', resultText);
+            res.download('Ответ на запрос.txt');
+        } catch (err) {
+            console.error(err);
+            res.status(500).send('Error saving the file');
+        }
+    } else {
+        res.status(405).send('Method Not Allowed');
     }
-
-    if (!isTextFile(jsonData.file1) || !isTextFile(jsonData.file2)) {
-        return res.status(400).json({ error: 'Один или оба файла не являются текстовыми' });
-    }
-
-    const file1Encoding = Buffer.from(jsonData.file1, 'base64').toString('utf-8');
-    const file2Encoding = Buffer.from(jsonData.file2, 'base64').toString('utf-8');
-
-    const similarityScore = calculateSimilarity(file1Encoding, file2Encoding);
-
-    const resultText = `Для запроса "${jsonData.text}" сходство (расстояние) равно ${similarityScore}`;
-
-    const jsonResponse = {
-        text: jsonData.text,
-        similarity: similarityScore,
-        result: resultText
-    };
-
-    res.json(jsonResponse);
 });
 
+
+app.all('/api',((req: Request, res: Response) => {
+    try {
+        if (req.method === 'GET') {
+            res.json(JsonResponse());
+        } else if (req.method === 'POST') {
+            const jsonData = req.body;
+
+            if (!jsonData.text || !jsonData.file1 || !jsonData.file2) {
+                return res.status(400).json({ error: 'Отсутствуют необходимые поля' });
+            }
+
+            if (!isTextFile(jsonData.file1) || !isTextFile(jsonData.file2)) {
+                return res.status(400).json({ error: 'Один или оба файла не являются текстовыми' });
+            }
+
+            const file1Encoding = Buffer.from(jsonData.file1, 'base64').toString('utf-8');
+            const file2Encoding = Buffer.from(jsonData.file2, 'base64').toString('utf-8');
+
+            const similarityScore = calculateSimilarity(file1Encoding, file2Encoding);
+
+            const resultText = `Для запроса "${jsonData.text}" сходство (расстояние) равно ${similarityScore}`;
+
+            const jsonResponse = {
+                text: jsonData.text,
+                similarity: similarityScore,
+                result: resultText
+            };
+
+            res.json(jsonResponse);
+    }
+        } catch(e)  {
+            res.status(405).send('Method Not Allowed');
+        }
+}))
 
 
 app.listen(port, () => {
